3003a15cfb6135892779863c6f023b16dd6a255e
ChrisCodeH <chrishowarduk@live.co.uk>
2017-02-22 17:18:46 +0000

    Created Window Animations on Click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 function main(){
-$(".carousel").hide();
-$(".carousel").fadeIn(2000);
+//$(".carousel").hide();
+//$(".carousel").fadeIn(2000);
+var pos = 1;
+$(".carousel").click(function(){
+    if(pos == 1)
+    {
+      $(".carousel").animate({"margin-left": "-100%"},1000);
+      pos = 2;
+    }
+    else if(pos == 2)
+    {
+      $(".carousel").animate({"margin-left": "-200%"},1000);
+      pos = 3;
+    }
+    else {
+      $(".carousel").animate({"margin-left": "0"},1000);
+      pos = 1;
+    }
+});
+
 }
 
 $(document).ready(main);
